c92490b32290dde45c9b925f69c81b90ee54285c
marshalWilliam <marshalwilliam500@gmail.com>
2026-09-08 15:31:55 +0800

    test(klaviyo): co-locate the ledger search test

--- src/components/blocks/attribution/klaviyo/ledger/ledger-search.component.test.tsx
+++ src/components/blocks/attribution/klaviyo/ledger/ledger-search.component.test.tsx
@@ -1,6 +1,6 @@
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
-import { LedgerSearch } from "./ledger/ledger-search";
+import { LedgerSearch } from "./ledger-search";
 
 beforeEach(() => {
   vi.useFakeTimers();
